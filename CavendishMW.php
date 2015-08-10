e5b5e4229b29a68e4a3d042bed62c200d3af8e2e
zariuq <zariuq@gmail.com>
2015-08-10 09:48:06 +0200

    Update CavendishMW.php

--- CavendishMW.php
+++ CavendishMW.php
@@ -11,7 +11,7 @@
  * Unported License. To view a copy of this license, visit
  * http://creativecommons.org/licenses/by-sa/3.0/ or send a letter to Creative
  * Commons, 171 Second Street, Suite 300, San Francisco, California, 94105, USA.
- *
+ * Located in public_html/wiki/wikihome/skins on cPanel
  */
 
 if( !defined( 'MEDIAWIKI' ) )
